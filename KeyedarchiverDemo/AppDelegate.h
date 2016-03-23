aecc0e83043685988230360bfcbb582a792b3ff2
岳文 <774655048@qq.com>
2016-03-23 08:41:29 +0800

    Initial Commit

---
+++ KeyedarchiverDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KeyedarchiverDemo
+//
+//  Created by YueWen on 16/3/23.
+//  Copyright © 2016年 YueWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KeyedarchiverDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KeyedarchiverDemo
+//
+//  Created by YueWen on 16/3/23.
+//  Copyright © 2016年 YueWen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
